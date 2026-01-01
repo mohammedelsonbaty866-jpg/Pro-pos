7537c17095ef18a3fed8abc53b3341662d3ea1f8
mohammedelsonbaty866-jpg <251473411+mohammedelsonbaty866-jpg@users.noreply.github.com>
2026-01-01 17:09:17 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -35,3 +35,40 @@ self.addEventListener("fetch", e => {
     })
   );
 });
+const CACHE_NAME = "pospro-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html"
+];
+
+// INSTALL
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// ACTIVATE
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
