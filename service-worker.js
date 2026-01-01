80c615e4bcf696846e8f5ca688364f202cc27b49
mohammedelsonbaty866-jpg <251473411+mohammedelsonbaty866-jpg@users.noreply.github.com>
2026-01-01 23:25:30 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,74 +1,17 @@
-const CACHE_NAME = "pospro-cache-v1";
-
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html"
+const CACHE = "pos-pro-v1";
+const ASSETS = [
+  "./",
+  "./index.html"
 ];
 
-// INSTALL
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
-// ACTIVATE
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// FETCH
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
-});
-const CACHE_NAME = "pospro-cache-v1";
-
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html"
-];
-
-// INSTALL
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
-});
-
-// ACTIVATE
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// FETCH
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
