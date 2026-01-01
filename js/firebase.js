ceede0fe5203639beb8095a69dfd289ad64239cc
mohammedelsonbaty866-jpg <251473411+mohammedelsonbaty866-jpg@users.noreply.github.com>
2026-01-01 15:45:15 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 
-// Your web app's Firebase configuration
+import { getAuth } from
+  "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+import { getFirestore } from
+  "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyChQFntrb_ewOFUl4wssdL8k_0kvlndTFQ",
   authDomain: "flutter-ai-playground-af830.firebaseapp.com",
   projectId: "flutter-ai-playground-af830",
-  storageBucket: "flutter-ai-playground-af830.firebasestorage.app",
+  storageBucket: "flutter-ai-playground-af830.appspot.com",
   messagingSenderId: "957185649772",
   appId: "1:957185649772:web:c87254acedaf7ae85dbc49"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db   = getFirestore(app);
